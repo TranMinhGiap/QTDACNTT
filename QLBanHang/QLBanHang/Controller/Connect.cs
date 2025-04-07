18d8e5abba01a62aecf278b80b521cf837e8321b
Tran Minh Giap <giap1519gn@gmail.com>
2025-04-07 20:34:27 +0700

    Complete

---
+++ QLBanHang/QLBanHang/Controller/Connect.cs
@@ -0,0 +1,25 @@
+﻿using QLBanHang.View;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanHang.Controller
+{
+    public class Connect
+    {
+        public SqlConnection connect;
+        private string sql = "Data Source=LAPTOP-KSS8GTGS;Initial Catalog=QLBanHang2;User ID=sa; Password = 15092004";
+        public Connect()
+        {
+            connect = new SqlConnection(sql);
+        }
+        public SqlConnection GetConnect()
+        {
+            return connect;
+        }
+    }
+}
